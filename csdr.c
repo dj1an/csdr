71dc154701ae991a27dbd3927550a7a364d8f9f6
dj1an <dj1an@users.noreply.github.com>
2020-04-06 12:19:45 +0200

    Update csdr.c

--- csdr.c
+++ csdr.c
@@ -29,7 +29,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
  
 #define _POSIX_C_SOURCE 199309L
-#define _BSD_SOURCE
+#define _DEFAULT_SOURCE
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
